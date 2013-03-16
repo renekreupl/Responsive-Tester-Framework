a750911295c1cc31f8a5b11879a6b13ccff631ca
Rene Kreupl <info@renekreupl.de>
2013-03-16 01:04:04 +0100

    The start

---
+++ incl/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Responsive Tester Framework
+*
+* @author Rene Kreupl <info@renekreupl.de>
+* @copyright Copyright (c) 2013 Rene Kreupl
+* @license http://github.com/sqmk/Phue/wiki/License
+* @package responsive-tester
+*/
+
+//Geräte Auflösung Array
+$devices[] = array("title" => "iPhone3", "width" => "365", "height" => "1024");
+
+// Config
+// 15px + ja nein
+
+// Do no changes
+require_once("class.device.php");
